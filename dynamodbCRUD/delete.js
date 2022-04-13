464729feecaa05768a0c95e2b711ab01a7d372ef
Elias <46169780+hedge0@users.noreply.github.com>
2022-04-12 23:21:38 -0400

    Update delete.js

--- dynamodbCRUD/delete.js
+++ dynamodbCRUD/delete.js
@@ -1,4 +1,4 @@
-let AWS = require("aws-sdk");
+const AWS = require("aws-sdk");
 AWS.config.update({ region: "us-east-1" });
 const DynamoDB = new AWS.DynamoDB.DocumentClient();
 const tableName = 'NEW_TABLE';
